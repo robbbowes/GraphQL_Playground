b4a0371446b62349b1b523f6a6e2b9734077de6a
robbbowes <up666724@myport.ac.uk>
2019-05-12 13:12:53 +0200

    Set up express server, schema and booktype

---
+++ server/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  // schema: schema //ES5
+  schema //ES6
+}));
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+  console.log('Listening on port ' + PORT)
+});
