93527d7368181dc796efc4db6e0da8fae51aeb4d
robbbowes <up666724@myport.ac.uk>
2019-05-16 20:20:54 +0200

    addBookMutation working, start with BookDetails functionality

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
-import {getBooksQuery} from '../queries/queries';
+import { getBooksQuery } from '../queries/queries';
+
+//components
+import BookDetails from './BookDetails';
 
 class BookList extends Component {
 
@@ -25,6 +28,7 @@ class BookList extends Component {
         <ul id='book-list'>
           {this.displayBooks()}
         </ul>
+        <BookDetails />
       </div>
     );
   }
