e0dfba354afb9ed68e0610fc69dcb45ce52c911d
robbbowes <up666724@myport.ac.uk>
2019-05-16 16:40:10 +0200

    Externalise then import queries, create AddBook component and beginnings of functionality

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
-import { gql } from 'apollo-boost';
 import { graphql } from 'react-apollo';
-
-const getBooksQuery = gql`
-  {
-    books {
-      name
-      id
-    }
-  }
-`
+import {getBooksQuery} from '../queries/queries';
 
 class BookList extends Component {
+
+  displayBooks() {
+    var data = this.props.data;
+    if (data.loading) {
+      return (
+        <div>Loading books...</div>
+      )
+    } else {
+      return data.books.map(book => {
+        return (
+          <li key={book.id}>{book.name}</li>
+        )
+      })
+    }
+  }
+
   render() {
-    console.log(this.props);
     return (
       <div>
         <ul id='book-list'>
-          <li>Test</li>
+          {this.displayBooks()}
         </ul>
       </div>
     );
